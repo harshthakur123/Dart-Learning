eba519d97637939186d11c24f48dd224f250a3cf
harshthakur123 <thakurharshsingh99@gmail.com>
2024-09-27 22:07:40 +0530

    Environment Setup

--- 01_Basics/01_Environment_Setup.dart
+++ 01_Basics/01_Environment_Setup.dart
@@ -1,3 +1,29 @@
-void main(){
-  print("Hello");
-}
+// void main(){
+//   print("Hello");
+// }
+
+// void main(){
+// print();
+// } 
+
+// void hello(){
+//   print("hk");
+// }
+
+// void main(){
+
+//   void hello1(){
+//     print("Helllo 1");
+//   }
+
+//   hello1();
+
+//   void hello2(){
+//     print("Hello 2");
+//   }
+
+//   hello2();
+
+// }
+
+
